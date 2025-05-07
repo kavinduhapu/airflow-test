660f7ab789140ae7c1081be3bfa7fd1396f55779
kavinduhapu <hapurox@gmail.com>
2025-05-07 22:20:21 +0530

    Add sample utility function

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+
+def sample_utility_function():
+    """
+    This is a sample utility function.
+    It doesn't do anything useful, but it's here to demonstrate the structure.
+    """
+    return True
