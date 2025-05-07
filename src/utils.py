30512328dda01d5204b8681a09428ad3579a7267
kavinduhapu <hapurox@gmail.com>
2025-05-07 22:59:43 +0530

    Add datafetch and preprocess functions

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,58 @@ def sample_utility_function():
     It doesn't do anything useful, but it's here to demonstrate the structure.
     """
     return True
+
+
+def fetch_data():
+    import os
+    import pandas as pd
+    from sklearn.datasets import load_iris
+
+    from src.utils import sample_utility_function
+    print(sample_utility_function())
+
+    BASE_DIR = "/tmp/airflow_iris"
+
+    os.makedirs(BASE_DIR, exist_ok=True)
+    iris = load_iris()
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    df['target'] = iris.target
+
+    data_path = os.path.join(BASE_DIR, "iris_data.csv")
+    df.to_csv(data_path, index=False)
+
+    # ti.xcom_push(key='data_path', value=data_path)
+    # return {"data_path": data_path}
+    return data_path
+
+
+def preprocess_data(data_path):
+    import os
+    import pickle
+    import pandas as pd
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    # data_path = ti.xcom_pull(task_ids='fetch_data')['data_path']
+    print(data_path)
+    df = pd.read_csv(data_path)
+
+    print(df.head())
+
+    X = df.drop('target', axis=1)
+    y = df['target']
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, test_size=0.2, random_state=42
+    )
+
+    BASE_DIR = "/tmp/airflow_iris"
+    processed_path = os.path.join(BASE_DIR, "processed.pkl")
+    with open(processed_path, 'wb') as f:
+        pickle.dump((X_train, X_test, y_train, y_test), f)
+
+    # ti.xcom_push(key='processed_path', value=processed_path)
+
+    return {"processed_path": processed_path}
